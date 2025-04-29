eb87aae73a61ed0b06eb6aed36cf067e982f099d
25351ap <25351ap@hvhs.school.nz>
2025-04-29 23:54:23 +0000

    Apr 29, 2025, 11:54 PM

--- t02_variables.js
+++ t02_variables.js
@@ -5,8 +5,6 @@
  Learing how to do variables
  ****************************/
  console.log("Running t02_variables.js");
- console.log("Hi Jerimah the second");
- console.log("");
  //Variables
  let userAge = 15
   let userPocketMoney = 72
