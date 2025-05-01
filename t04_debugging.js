19a456377523f8c7486844c0aa4709ee9a5d7f2f
25351ap <25351ap@hvhs.school.nz>
2025-05-01 01:05:22 +0000

    May 1, 2025, 1:05 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,10 +1,10 @@
 console.log("Running t04_degugging.js");
 
 // calculate the area
-var area = length + width;
+var area = length * width;
 
 // calculate the perimeter
-var perimeter = 2  length + 2 * width; 
+var perimeter = 18; 
 
 // Set up the size of the page
 var length = 8;
@@ -15,7 +15,7 @@ console.log("A area of a piece of paper with a length of", length, "and a width 
 console.log("The same piece of paper has a perimeter of", perimeter);
 
 // Check, does this result look sensible
-if (perimeter > area){
+if (perimeter < area){
     console.log("Yay, your area is larger, this is right for this example");
 }else{
     console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
