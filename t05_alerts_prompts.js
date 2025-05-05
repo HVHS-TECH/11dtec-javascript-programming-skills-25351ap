31e0ebdccf21cddfc08fb3cea6afe57e6ce79ae2
25351ap <25351ap@hvhs.school.nz>
2025-05-05 21:04:46 +0000

    May 5, 2025, 9:04 PM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -14,14 +14,14 @@
  /****************************
   Main code
   ****************************/
-  console.log("Hi Jerimah the second");
+  console.log("Hi", username);
   console.log("As of", currentYear, "you are", userAge, "years old");
   console.log("You were born in", currentYear - userAge);
   console.log("In 10 years you will be", Number(userAge) + 10, "years old");
   console.log("You have", userPocketMoney, "dollars");
   console.log("Half of your money is", userPocketMoney / 2, "dollars");
   alert("Welcome user to my first Javascript program!");
-  alert("Hi username", "/nAs of", currentYear, "you are", userAge, "years old", "/nYou were born in", currentYear - userAge, "/nIn 10 years you will be", Number(userAge) + 10, "years old", "/nYou have", userPocketMoney, "dollars", "/nHalf of your money is", userPocketMoney / 2);
+  alert("Hi", username, "/nAs of", currentYear, "you are", userAge, "years old", "/nYou were born in", currentYear - userAge, "/nIn 10 years you will be", Number(userAge) + 10, "years old", "/nYou have", userPocketMoney, "dollars", "/nHalf of your money is", userPocketMoney / 2);
   /****************************
   Functions
   ****************************/
