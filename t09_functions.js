80afaa629651e6b0a5fa43ba4f3b5b529d2a2396
25351ap <25351ap@hvhs.school.nz>
2025-05-13 23:57:11 +0000

    May 13, 2025, 11:57 PM

---
+++ t09_functions.js
@@ -0,0 +1,29 @@
+/****************************
+ Task 9
+ Alice
+ 14-05-2025
+ Learing how to use functions in javascript
+ ****************************/
+console.log("Running t08_for_loop.js")
+
+//Variables
+let randomnum = 0;
+
+/****************************
+ Main code
+ ****************************/
+console.log("Starting loop")
+for (count = 0; count < 5; count ++) {
+    let currentnum = prompt ("Type a random number", 0);
+    randomnum = randomnum + (Number(currentnum));
+}
+console.log("Loop stopped!")
+console.log("Your random number is " + randomnum)
+
+
+ /****************************
+ Functions
+ ****************************/
+function askUserName() {
+    
+}
