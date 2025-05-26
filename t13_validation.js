01b673409eff7183dc798b13a0d0de9fff930fb1
25351ap <25351ap@hvhs.school.nz>
2025-05-26 21:15:21 +0000

    May 26, 2025, 9:15 PM

---
+++ t13_validation.js
@@ -0,0 +1,50 @@
+/****************************
+ Task 13
+ Alice
+ 27-05-2025
+ Learing to only allow valid input from the user
+ ****************************/
+console.log("Running t13_validation.js")
+
+//Variables
+let randomnum = 0;
+let UserName;
+let UserAge;
+let UserPocketMoney;
+
+/****************************
+ Main code
+ ****************************/
+console.log("Starting loop")
+for (count = 0; count < 5; count ++) {
+    let currentnum = prompt ("Type a random number", 0);
+    randomnum = randomnum + (Number(currentnum));
+}
+console.log("Loop stopped!")
+console.log("Your random number is " + randomnum)
+function start() {
+    UserName = askUserName();
+    UserAge = askUserAge();
+    UserPocketMoney = askUserMoney();
+}
+
+
+ /****************************
+ Functions
+ ****************************/
+function askUserName() {
+    UserName = prompt("What is your name?");
+    alert("Welcome to my program " + UserName + "!");
+}
+function askUserAge() {
+    UserAge = prompt("How old are you?");
+    alert("You are " + UserAge + " years old.");
+}
+function askUserMoney() {
+    UserPocketMoney = prompt("How much pocket money do you have?");
+    if (UserPocketMoney > 4){
+    alert("You have " + UserPocketMoney + " dollars. That is enough to purchase a 4 dollar chocolate bar. YAY!");
+  } else {
+    alert("You have " + UserPocketMoney + " dollars. That is not enough to purchase a 4 dollar chocolate bar. :(");
+  }
+}
